03e947e3ad8658478e43c72729229d8605248c40
MorZLE <art04.04@mail.ru>
2023-01-11 20:22:46 +0300

    мой 2 комит

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
+import psycopg2
 def main():
-  x=1
-  x=2
+  con = psycopg2.connect(
+    database="demo",
+    user="postgres",
+    password="111111",
+    host="127.0.0.1",
+    port="5432"
+  )
+
+  print("База подключена")
+  cur = con.cursor()
+  cur.execute("SELECT table_name FROM information_schema.tables WHERE table_schema='public' ")
+
+  rows = cur.fetchall()
+  for row in rows:
+    print(*row)
+  con.close()
+  print("База отключена")
   pass
 
 if __name__ == '__main__':
