ebcc0d0861b0fd965d8c5f6c3512a8c5c8e6096d
MorZLE <art04.04@mail.ru>
2023-01-15 19:27:45 +0300

    задание 4 1 комит

--- main.py
+++ main.py
@@ -1,21 +1,46 @@
 import psycopg2
+import mysql.connector
 from prettytable import PrettyTable
 
-def main():
-  print('Если хотите посмотреть название таблиц, то введите ключ -a после названия файла')
-  print('Введите \q для выхода')
-  input_data = input("Введите название файла: ").split()
 
-  if len(input_data) > 1:
+def main():
+  print('Если хотите посмотреть название таблиц, то введите ключ -a после названия файла, по умолчанию введите -p')
+  print('Введите \q для выхода')
+
+  try:
+   input_data = input("Введите название файла, ключ и тип базы: ").split()
+  except KeyboardInterrupt:
+    print('\nПрограмма закрыта')
+    exit(0)
+
+  try:
     file_name = input_data[0]
     key = input_data[1]
-  else:
-    file_name = input_data[0]
-    key = ''
+    type_bd=input_data[2]
+  except IndexError:
+    print('Нехватает данных, программа закрыта')
+    exit(0)
 
+  if type_bd=='postgres':
+    connection_postgres(file_name)
+  elif type_bd=='MySQL':
+    connection_MySQL(file_name)
+  else:
+    print('Данный тип подключения еще поддерживается\nПрограмма закрыта')
+    exit(0)
+
+  if key == '-a':
+    print('Cписок доступных таблиц:')
+    execute_query(connection, "SELECT table_name FROM information_schema.tables WHERE table_schema='public'")
+    sql_request(connection)
+
+  sql_request(connection)
+
+def connection_postgres(file_name):
   try:
     with open(file_name, encoding='UTF-8') as file:
       try:
+          global connection
           connection = psycopg2.connect(
           database=file.readline().strip(),
           user=file.readline().strip(),
@@ -24,9 +49,7 @@ def main():
           port=file.readline().strip()
         )
       except psycopg2.OperationalError:
-        print("Некорректные данные")
-        connection.close()
-        print("База отключена")
+        print("Некорректные данные\nПрограмма закрыта")
         exit(0)
       else:
         print("База подключена")
@@ -34,35 +57,46 @@ def main():
     print("Файл не найден")
     exit(0)
 
-  if key=='-a':
-    print('Cписок доступных таблиц:')
-    execute_query(connection, "SELECT table_name FROM information_schema.tables WHERE table_schema='public'")
-    sql_request(connection)
-  else:
-    sql_request(connection)
-
-def sql_request(connection):###функция сбора запроса
+def connection_MySQL(file_name):
   try:
-    request_txt=''
+    with open(file_name, encoding='UTF-8') as file:
+      try:
+          global connection
+          connection = mysql.connector.connect(
+          host=file.readline().strip(),
+          user=file.readline().strip(),
+          passwd=file.readline().strip(),
+
+        )
+      except Exception: #найти ошибку
+         print("Некорректные данные\nПрограмма закрыта")
+         exit(0)
+      else:
+         print("База подключена")
+  except FileNotFoundError:
+    print("Файл не найден")
+    exit(0)
+def sql_request(connection):
+  '''функция сбора запроса'''
+  try:
     request_sql = ''
     while True:
-      request_txt = input()
-      if request_txt[:6]=="SQL >>":
-        request_sql += request_txt[7:]
-        if request_sql[-1] == ';':
-          execute_query(connection, request_sql)
-          request_sql = ''
-      elif request_txt[-2:] == '\q':
-        print('Программа закрыта')
+      request_sql += input('SQL >> ')
+      if request_sql[-1] == ';':
+        execute_query(connection, request_sql)
+        request_sql = ''
+      elif request_sql[-2:] == '\q':
+        print('\nПрограмма закрыта')
         connection.close()
         print("База отключена")
         exit(0)
   except KeyboardInterrupt:
-    print('Программа закрыта')
+    print('\nПрограмма закрыта')
     connection.close()
     print("База отключена")
 
-def execute_query(connection, query): ###функция отправки запроса
+def execute_query(connection, query):
+  '''функция отправки запроса'''
   cursor = connection.cursor()
   try:
     cursor.execute(query)
@@ -72,8 +106,11 @@ def execute_query(connection, query): ###функция отправки зап�
     for row in result:
       t.add_row([*row])
     print(t)
-  except psycopg2.ProgrammingError:
-    print('Нету данных для вывода!')
+  except psycopg2.ProgrammingError as err:
+    if 'no results to fetch' in str(err):
+      print('Нету данных для вывода!')
+    else:
+      print(err)
 
 
 if __name__ == '__main__':
