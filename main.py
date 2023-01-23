5eb4c964f1e97b75b478afef429f4d4945ba7b0e
MorZLE <art04.04@mail.ru>
2023-01-23 19:45:27 +0300

    классы: фикс 5 комит

--- main.py
+++ main.py
@@ -4,41 +4,13 @@ import pyodbc
 from prettytable import PrettyTable
 
 
-def main():
-  print('Если хотите посмотреть название таблиц, то введите ключ -a после названия файла, по умолчанию введите -p')
-  print('Введите \q для выхода')
-
-  try:
-   print("Введите название файла, ключ и номер вендора 1-postgres, 2-MySQL, 3-MSserver:")
-   input_data = input().split()
-  except KeyboardInterrupt:
-    print('\nПрограмма закрыта')
-    exit(0)
-
-  try:
-    file_name = input_data[0]
-    key = input_data[1]
-    type_bd=input_data[2]
-    user=DB(file_name,type_bd)
-    connection=user.DB_connection(file_name,type_bd)
-  except IndexError:
-    print('Нехватает данных, программа закрыта')
-    exit(0)
-
-  if key == '-a':
-    print('Cписок доступных таблиц:')
-    user.execute_query(connection, "SELECT table_name FROM information_schema.tables WHERE table_schema='public'")
-    user.sql_request(connection)
-
-  user.sql_request(connection)
-
 class DB:
-  def __init__(self,file_name,type_bd):
-    self.file_name=file_name
-    self.type_bd=type_bd
-    self.connection=None
+  def __init__(self, file_name, type_bd):
+    self.file_name = file_name
+    self.type_bd = type_bd
+    self.connection = None
 
-  def DB_connection(self,file_name,type_bd):
+  def DB_connection(self, file_name, type_bd):
     try:
       with open(self.file_name, encoding='UTF-8') as file:
         try:
@@ -49,23 +21,23 @@ class DB:
               password=file.readline().strip(),
               host=file.readline().strip(),
               port=file.readline().strip())
-            return self.connection
-          elif  self.type_bd == '2':
 
+          elif self.type_bd == '2':
             self.connection = mysql.connector.connect(
-            host=file.readline().strip(),
-            user=file.readline().strip(),
-            passwd=file.readline().strip(),
-            db=file.readline().strip())
-            return self.connection
-          elif  self.type_bd == '3':
+              host=file.readline().strip(),
+              user=file.readline().strip(),
+              passwd=file.readline().strip(),
+              db=file.readline().strip())
+
+          elif self.type_bd == '3':
             self.connection = pyodbc.connect(f"Driver={file.readline().strip()};"
-                                      f"Server={file.readline().strip()};"
-                                      f"Database={file.readline().strip()};"
-                                      f"Trusted_Connection={file.readline().strip()};")
-            return self.connection
+                                             f"Server={file.readline().strip()};"
+                                             f"Database={file.readline().strip()};"
+                                             f"Trusted_Connection={file.readline().strip()};")
+
           else:
-            print('Вендор в данный момент не поддерживается. Список доступных вендоров: 1-postgres, 2-MySQL, 3-MSserver')
+            print(
+              'Вендор в данный момент не поддерживается. Список доступных вендоров: 1-postgres, 2-MySQL, 3-MSserver')
             exit(0)
 
         except (psycopg2.OperationalError, mysql.connector.errors.DatabaseError, pyodbc.InterfaceError):
@@ -77,7 +49,8 @@ class DB:
     except FileNotFoundError:
       print("Файл не найден")
       exit(0)
-  def sql_request(self,connection):
+
+  def sql_request(self):
     '''функция сбора запроса'''
     try:
       request_sql = ''
@@ -96,13 +69,13 @@ class DB:
       self.connection.close()
       print("База отключена")
 
-  def execute_query(self,connection, query):
+  def execute_query(self, query):
     '''функция отправки запроса'''
     cursor = self.connection.cursor()
     try:
       cursor.execute(query)
       self.connection.commit()
-      result=cursor.fetchall()
+      result = cursor.fetchall()
       t = PrettyTable([description[0] for description in cursor.description])
       for row in result:
         t.add_row([*row])
@@ -112,6 +85,34 @@ class DB:
         print('Нету данных для вывода!')
       else:
         print(err)
+
+
+def main():
+  print('Если хотите посмотреть название таблиц, то введите ключ -a после названия файла, по умолчанию введите -p')
+  print('Введите \q для выхода')
+
+  try:
+   print("Введите название файла, ключ и номер вендора 1-postgres, 2-MySQL, 3-MSserver:")
+   input_data = input().split()
+  except KeyboardInterrupt:
+    print('\nПрограмма закрыта')
+    exit(0)
+
+  try:
+    file_name = input_data[0]
+    key = input_data[1]
+    type_bd=input_data[2]
+    user=DB(file_name,type_bd)
+    user.DB_connection(file_name,type_bd)
+  except IndexError:
+    print('Нехватает данных, программа закрыта')
+    exit(0)
+
+  if key == '-a':
+    print('Cписок доступных таблиц:')
+    user.execute_query("SELECT table_name FROM information_schema.tables WHERE table_schema='public'")
+
+  user.sql_request()
 
 
 if __name__ == '__main__':
