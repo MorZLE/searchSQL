cd8900b3a01b6633df74553a473d7b7b25f296e6
MorZLE <art04.04@mail.ru>
2023-03-09 10:15:09 +0300

    2.9.4 страница 404

--- web_version.py
+++ web_version.py
@@ -157,7 +157,9 @@ def logout():
 def test():
     return render_template('test.html')
 
-
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('error.html'), 404
 
 
 if __name__ == '__main__':
